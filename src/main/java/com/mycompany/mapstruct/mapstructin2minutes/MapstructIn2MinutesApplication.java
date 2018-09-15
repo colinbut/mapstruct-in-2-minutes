6862032f38296d79f13dd9d7acb3c75b1d93db99
Colin But <colinbut@users.noreply.github.com>
2018-09-15 13:33:28 +0100

    Project Template generated from Spring Initializer

---
+++ src/main/java/com/mycompany/mapstruct/mapstructin2minutes/MapstructIn2MinutesApplication.java
@@ -0,0 +1,12 @@
+package com.mycompany.mapstruct.mapstructin2minutes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MapstructIn2MinutesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MapstructIn2MinutesApplication.class, args);
+	}
+}
---
+++ src/test/java/com/mycompany/mapstruct/mapstructin2minutes/MapstructIn2MinutesApplicationTests.java
@@ -0,0 +1,16 @@
+package com.mycompany.mapstruct.mapstructin2minutes;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MapstructIn2MinutesApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
